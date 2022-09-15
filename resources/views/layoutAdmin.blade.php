4babbed8b7f5cc3efab700868d09731aee79a002
Saparot <coding@saparot.io>
2022-09-15 21:39:14 +0200

    use font IBMPlexMono added plants create link

--- resources/views/layoutAdmin.blade.php
+++ resources/views/layoutAdmin.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <style>
+        @font-face {
+            font-family: 'IBMPlexMono';
+            src: url({{ asset('fonts/IBMPlexMono-Regular.ttf')  }}) format('truetype');
+        }
+    </style>
+
     <link rel="stylesheet" href="{{ asset('temp/reset.css') }}">
     <link rel="stylesheet" href="{{ asset('temp/main.css') }}">
     <title>@yield('title','garden api admin')</title>
@@ -11,6 +18,12 @@
         <li class="navigation__item {{ request()->routeIs('admin.home') ? 'navigation__item--active' : '' }}">
             <a href="{{ route('admin.home') }}">admin home</a>
         </li>
+
+
+        <li class="navigation__item {{ request()->routeIs('admin.plants.create') ? 'navigation__item--active' : '' }}">
+            <a href="{{ route('admin.plants.create') }}">plant create</a>
+        </li>
+
         <li class="navigation__item {{ request()->routeIs('public.home') ? 'navigation__item--active'  : ''  }}">
             <a href="{{ route('public.home')  }}">public home</a>
         </li>
