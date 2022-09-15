60e5289f503e5d00954bb75a6c6f8c441064cc6d
Saparot <coding@saparot.io>
2022-09-15 21:47:06 +0200

    added admin plants routes create, store reordered routes

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->group(function () {
-
-    Route::get('/', static fn() => view('admin.home'))->name('admin.home');
-
-});
-
 Route::get('/', static fn() => view('public.home'))->name('public.home');
 
 Route::get('/about', static fn() => view('public.about'))->name('public.about');
 
+Route::prefix('admin')->group(function () {
+    Route::get('/', static fn() => view('admin.home'))->name('admin.home');
+
+    Route::get('/plants/create', static fn() => view('admin.plants.create'))->name('admin.plants.create');
+
+    Route::post('/plants/store', static fn() => view('admin.plants.store'))->name('admin.plants.store');
+});
